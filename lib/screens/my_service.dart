6894ddc5b0617248a0090b26848267b425f2a0fb
masterUNG <phrombutr@gmail.com>
2019-08-15 14:10:56 +0700

    update2

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ung_qr_code/screens/home.dart';
 
 class MyService extends StatefulWidget {
   @override
@@ -26,14 +27,17 @@ class _MyServiceState extends State<MyService> {
   }
 
   Widget showLogin() {
-    return Text('Login by $nameLogin');
+    return Text(
+      'Login by $nameLogin',
+      style: TextStyle(color: Colors.white),
+    );
   }
 
   Widget showAppName() {
     return Text(
       'Ung QR code',
       style: TextStyle(
-          fontSize: 18.0, color: Colors.purple, fontFamily: 'IndieFlower'),
+          fontSize: 18.0, color: Colors.white, fontFamily: 'IndieFlower'),
     );
   }
 
@@ -49,8 +53,7 @@ class _MyServiceState extends State<MyService> {
     return DrawerHeader(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('images/land.jpg'),fit: BoxFit.fill
-        ),
+            image: AssetImage('images/land.jpg'), fit: BoxFit.fill),
       ),
       child: Column(
         children: <Widget>[
@@ -62,11 +65,63 @@ class _MyServiceState extends State<MyService> {
     );
   }
 
+  Widget pageFirstMenu() {
+    return ListTile(
+      leading: Icon(Icons.home),
+      title: Text('First Page'),
+      subtitle: Text('นี่คือหน้าแสดง ListView'),
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  Widget pageSecondMenu() {
+    return ListTile(
+      leading: Icon(Icons.android),
+      title: Text('Second Page'),
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  Widget pageSignOutMenu() {
+    return ListTile(
+      leading: Icon(Icons.exit_to_app),
+      title: Text('Sign Out'),
+      onTap: () {
+        Navigator.of(context).pop();
+        mySignOut();
+      },
+    );
+  }
+
+  Future<void> mySignOut() async {
+    await firebaseAuth.signOut().then(
+      (response) {
+        MaterialPageRoute materialPageRoute =
+            MaterialPageRoute(builder: (BuildContext context) => Home());
+        Navigator.of(context).pushAndRemoveUntil(
+            materialPageRoute, (Route<dynamic> route) => false);
+      },
+    );
+  }
+
+  Widget myDivider() {
+    return Divider();
+  }
+
   Widget myDrawer() {
     return Drawer(
       child: ListView(
         children: <Widget>[
           myDrawerHeader(),
+          pageFirstMenu(),
+          myDivider(),
+          pageSecondMenu(),
+          myDivider(),
+          pageSignOutMenu(),
         ],
       ),
     );
